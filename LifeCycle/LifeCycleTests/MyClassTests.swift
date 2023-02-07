e49fe16877528873347539e6bc3f2093d6727063
Carlos Paredes <carlosparedes@Carloss-MacBook-Pro.local>
2023-02-07 08:45:12 -0600

    [ADDED] test fixtures

--- LifeCycle/LifeCycleTests/MyClassTests.swift
+++ LifeCycle/LifeCycleTests/MyClassTests.swift
@@ -6,9 +6,35 @@
 //
 
 import XCTest
+@testable import LifeCycle
 
 final class MyClassTests: XCTestCase {
-    func test_zero() {
-        XCTFail("Tests not yet implemented in MyClassTests")
+    /// Need to import @testable import LifeCycle
+    /// Why do we need the @testable attribute on the import statement?
+    /// That’s because we didn’t specify access control for MyClass,
+    /// so it defaulted to internal access. @testable makes internal declarations visible.
+    
+    private var sut: MyClass!
+    
+    override func setUp() {
+        super.setUp()
+        sut = MyClass()
     }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func test_methodOne() {
+        sut.methodOne()
+        // Normally, assert smth
+    }
+    
+    func test_methodTwo() {
+        sut.methodTwo()
+    }
+    
+    ///Unit Tests Are FIRST: Fast, Isolated, Repeatable, Self-Verifying, and Timely
+    
 }
