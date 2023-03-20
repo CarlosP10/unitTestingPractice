494ae797cf005caf5133f1dd54781b6b3719053d
Carlos Paredes <00028516@uca.edu.sv>
2023-03-20 16:44:46 -0600

    Chapter 7 - Init Project - [OutletConnections]

---
+++ OutletConnections/OutletConnections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OutletConnections
+//
+//  Created by Carlos Paredes on 20/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OutletConnections/README.md
@@ -0,0 +1,3 @@
+# OutletConnections
+
+### Key Takeaways
